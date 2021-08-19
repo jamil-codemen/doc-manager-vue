ca7a1a3605f69b141877c9865d30735a340c21ab
jamil-codemen <jamil@codemen.net>
2021-08-19 17:59:19 +0600

    Document Manager
    	1:A basic layout
    	2:Aws amplify hosting

---
+++ src/components/Home/CardSection.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="flex flex-wrap bg-white py-4 border-b-1 rounded-lg ">
+      <div class="w-4/12 flex justify-start border-r-2 border-gray-300">
+        <div class="p-12">
+          <div class="ml-8">
+            <img src="../../assets/static/h-icon-1-1.png" alt="" />
+          </div>
+          <div>
+            <p class="mt-2">Community Forum</p>
+          </div>
+        </div>
+      </div>
+      <div class="w-4/12 flex justify-center">
+        <div class="p-12">
+          <div class="ml-8">
+            <img src="../../assets/static/h-icon-2-1.png" alt="" />
+          </div>
+          <div>
+            <p class="mt-2">Documentation</p>
+          </div>
+        </div>
+      </div>
+      <div class="w-4/12 flex justify-end border-l-2 border-gray-300">
+        <div class="px-32 py-12">
+          <div class="ml-4">
+            <img src="../../assets/static/h-icon-3-1.png" alt="" />
+          </div>
+          <div>
+            <p class="mt-2">24/7 Support</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CardSection",
+};
+</script>
---
+++ src/components/Navigation/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="bg-blue-700 max-h-full">
+      <div class="container mx-auto relative">
+    <div class="flex flex-wrap">
+      <div class="w-6/12 mt-7 text-white">
+        logo
+      </div>
+      <div class="w-6/12">
+        <div class="flex flex-wrap space-x-10 mt-7 text-white">
+            <div>
+                Home
+            </div>
+            <div>
+                Documentation
+            </div>
+            <div>
+                Forum
+            </div>
+            <div>
+                Pages
+            </div>
+            <div>
+                Blog
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-wrap justify-center">
+        <div class="pt-48 pb-9">
+            <p class="text-5xl font-bold text-white">How can we help you?</p>
+        </div>
+    </div>
+      <div class="flex flex-wrap justify-center pb-48">
+        <div>
+            <input type="text" class="py-4 px-40 border rounded-lg inline-block" placeholder="Search for topics">
+        </div>
+        <div>
+            <button class="px-4 py-4 bg-blue-500 text-white rounded-lg mx-2">Search</button>
+        </div>
+    </div>
+<CardSection />
+  </div>
+</div>
+</template>
+<script>
+import CardSection from '@/components/Home/CardSection.vue'
+export default {
+  name: "Navbar",
+  components:{
+      CardSection
+  }
+};
+</script>
