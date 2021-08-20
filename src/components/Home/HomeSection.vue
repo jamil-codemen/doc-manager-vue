295800dbfa67856ae28bd95bf3862cc4ae063042
jamil-codemen <jamil@codemen.net>
2021-08-20 12:48:31 +0600

    Doc Manager
    	1:Design Fix

--- src/components/Home/HomeSection.vue
+++ src/components/Home/HomeSection.vue
@@ -21,42 +21,7 @@
           </div>
         </div>
         <div class="w-full md:w-6/12">
-        <div class="px-6 py-6 bg-green-50 mx-10 text-center space-y-3">
-            <p class="text-2xl text-gray-900">Live Chat</p>
-            <p class="text-gray-900">
-              Professionally cultivate one-to-one find customer service with
-              robust ideas.
-            </p>
-            <p class="text-sm text-blue-900">Go to help page</p>
-          </div>
-        </div>
-      </div>
-    </div>
-   
-       <div class="container md:mx-auto  mt-10">
-      <div class="flex flex-wrap">
-        <div class="w-full md:w-4/12 mb-6">
-          <div class="px-6 py-6 bg-pink-50 mx-10 text-center space-y-3">
-            <p class="text-2xl text-gray-900">Knowledge Base</p>
-            <p class="text-gray-900">
-              Professionally cultivate one-to-one find customer service with
-              robust ideas.
-            </p>
-            <p class="text-sm text-blue-900">Go to help page</p>
-          </div>
-        </div>
-        <div class="w-full md:w-4/12">
-        <div class="px-6 py-6 bg-green-50 mx-10 text-center space-y-3">
-            <p class="text-2xl text-gray-900">Live Chat</p>
-            <p class="text-gray-900">
-              Professionally cultivate one-to-one find customer service with
-              robust ideas.
-            </p>
-            <p class="text-sm text-blue-900">Go to help page</p>
-          </div>
-        </div>
-           <div class="w-full md:w-4/12">
-        <div class="px-6 py-6 bg-green-50 mx-10 text-center space-y-3">
+          <div class="px-6 py-6 bg-green-50 mx-10 text-center space-y-3">
             <p class="text-2xl text-gray-900">Live Chat</p>
             <p class="text-gray-900">
               Professionally cultivate one-to-one find customer service with
@@ -68,6 +33,49 @@
       </div>
     </div>
 
+    <div class="flex justify-center text-center mt-3">
+      <div class="space-y-4">
+        <p class="text-xl text-gray-900">Contents We Provide</p>
+        <p class="text-gray-900">
+          Our Progress is your success, collaboration will save your time and money
+        </p>
+      </div>
+    </div>
+
+    <div class="container md:mx-auto  mt-10">
+      <div class="flex flex-wrap">
+        <div class="w-full md:w-4/12 mb-6">
+          <div class="px-6 py-6 bg-pink-50 mx-10 text-center space-y-3">
+            <p class="text-2xl text-gray-900">Project Doc</p>
+            <p class="text-gray-900">
+              Professionally cultivate one-to-one find customer service with
+              robust ideas.
+            </p>
+            <p class="text-sm text-blue-900">Go to help page</p>
+          </div>
+        </div>
+        <div class="w-full md:w-4/12 mb-6">
+          <div class="px-6 py-6 bg-green-50 mx-10 text-center space-y-3">
+            <p class="text-2xl text-gray-900">Tutorials</p>
+            <p class="text-gray-900">
+              Professionally cultivate one-to-one find customer service with
+              robust ideas.
+            </p>
+            <p class="text-sm text-blue-900">Go to help page</p>
+          </div>
+        </div>
+        <div class="w-full md:w-4/12">
+          <div class="px-6 py-6 bg-purple-50 mx-10 text-center space-y-3">
+            <p class="text-2xl text-gray-900">Environment</p>
+            <p class="text-gray-900">
+              Professionally cultivate one-to-one find customer service with
+              robust ideas.
+            </p>
+            <p class="text-sm text-blue-900">Go to help page</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
